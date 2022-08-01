97066936434a2a11b7a3c4170cadcd0287266a0b
Priyanshi Sharma <priyanshisharma085@gmail.com>
2022-08-02 05:14:22 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,10 @@
+console.log("day1");
+
+
+// DAY 1-
+
+// what is Javascript?
+// what is the javascript run time environment?
+// what is execution context?
+// what is javascript Engine?
+// flow of data in engine?
